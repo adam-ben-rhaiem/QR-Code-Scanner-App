b0eb980ed9a655bbf06e53e1f15462dd21d9c40a
adam-ben-rhaiem <141167222+adam-ben-rhaiem@users.noreply.github.com>
2024-01-07 22:09:05 +0100

    Update scanner.dart

--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,151 +1,49 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/pages/login.dart';
+import 'package:flutter_login/pages/scanner.dart';
+import 'package:flutter_login/pages/services/auth_service.dart';
+import 'package:flutter_login/pages/signup.dart';
+import 'package:flutter_login/wrapper.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_login/pages/mainpage.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
-// import 'package:example_qr_coed_reader/scanner.dart';
-class Scanner extends StatefulWidget {
-  @override
-  _ScannerState createState() => _ScannerState();
-}
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    runApp(const MyApp());
+    
+ }
+ 
 
-class _ScannerState extends State<Scanner> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  dynamic controller;
-
-//open camera and scan
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
-  @override
+  
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text("Scanner"),
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: [
-          Column(
-            children: <Widget>[
-              Expanded(
-                flex: 5,
-                child: Stack(
-                  children: [
-                    QRView(
-                      key: qrKey,
-                      onQRViewCreated: _onQRViewCreated,
-                    ),
-                    Center(
-                      child: Container(
-                        width: 300,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.red,
-                            width: 4,
-                          ),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Text('Scan a code'),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+          create:(_) => AuthService())
+      ],
+      child: GetMaterialApp(
+        initialRoute: '/',
+      
+    
+        routes: {
+        '/': (context) =>  Wrapper(),
+        '/login': (context) => const Loginform(),
+        '/signup': (context) => const Signup(),
+        '/scanner': (context) =>  Scanner(),
+        '/mainpage': (context) => const MainPage(scannedData: '', scannedPrice: '',),    
+
+
+      }),
     );
+    
+    
   }
-
-  void _onQRViewCreated(QRViewController controller) async {
-    // Access Firestore instance
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) async {
-      controller.pauseCamera();
-      DocumentReference documentReference =
-          firestore.collection('uuuu').doc(scanData.code.toString());
-      DocumentSnapshot docSnapshot = await documentReference.get();
-      var data = docSnapshot.data() as Map<String, dynamic>?;
-      if (await canLaunchUrlString(scanData.code.toString())) {
-        await launchUrlString(scanData.code.toString());
-        controller.resumeCamera();
-      } else {
-        var Price; // Declare Price variable outside the if block
-        if (data != null) {
-          Price = data['Prix'] as String?;
-        }
-        if (Price != null) {
-          
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('This is a match'),
-                content: SingleChildScrollView(
-                  child: ListBody(
-                    children: <Widget>[
-                      Text('Article: ${scanData.code}'),
-                      Text('Prix: ${Price}'),
-                    ],
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('Ajouter à votre panier'),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MainPage(scannedData:scanData.code.toString(), scannedPrice: Price,)
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-            },
-          ).then((value) => controller.resumeCamera());
-        } else {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('Could not find the Article'),
-                content: SingleChildScrollView(
-                  child: ListBody(
-                    children: <Widget>[
-                      Text('Data: ${scanData.code}'),
-                    ],
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('Ok'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              );
-            },
-          ).then((value) => controller.resumeCamera());
-        }
-      }
-    });
-  }
+  
 }
